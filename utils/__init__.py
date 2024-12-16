9207a15501276cdab9f1ce2a2eba5dca92957b2a
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-12-16 13:24:17 +0000

    refactoring
    ignore ANN202, and also move COLS to __init__

---
+++ utils/__init__.py
@@ -0,0 +1,17 @@
+"""Contains various utility functions."""
+
+INDEX_COLS = ["sent_id", "word_id"]
+DEP_REL_COLS = ["word", "deprel", "head"]
+POS_TAG_COLS = ["word", "lemma", "ud_tag", "penn_tag"]
+CONLLU_COLS = [
+    "id",
+    "form",
+    "lemma",
+    "upos",
+    "xpos",
+    "feats",
+    "head",
+    "deprel",
+    "deps",
+    "misc",
+]

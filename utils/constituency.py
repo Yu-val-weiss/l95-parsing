3324df1e2c1a1c4b044c31b1571de51a2ae77470
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-12-17 15:20:57 +0000

    add clean_tree and wipe_empty_tags

--- utils/constituency.py
+++ utils/constituency.py
@@ -6,6 +6,56 @@ import pyperclip
 from nltk.tree import Tree
 
 
+def wipe_empty_tags(tree: Tree) -> Tree:
+    """Combine leaf nodes that had an empty POS with the one above.
+
+    Args:
+        tree (Tree): Tree to cleanup.
+
+    Returns:
+        Tree: Cleaned up tree.
+
+    """
+    new_tree = []
+    for subtree in tree:
+        if isinstance(subtree, Tree):
+            if subtree.label() == "_":
+                new_tree.append(subtree.leaves()[0])
+            else:
+                new_tree.append(wipe_empty_tags(subtree))
+        else:
+            new_tree.append(subtree)
+    return Tree(tree.label(), new_tree)
+
+
+def remove_top(tree: Tree) -> Tree:
+    """Remove "top" node in Tree.
+
+    Args:
+        tree (Tree): Tree to be cleaned.
+
+    Returns:
+        Tree: TOP-less tree.
+
+    """
+    if isinstance(tree.label(), str) and tree.label().lower() == "top":
+        return tree[0]  # type: ignore
+    return tree
+
+
+def clean_tree(tree: Tree) -> Tree:
+    """Clean tree for processing.
+
+    Args:
+        tree (Tree): Uncleaned up tree from parser.
+
+    Returns:
+        Tree: Clean tree.
+
+    """
+    return wipe_empty_tags(remove_top(tree))
+
+
 def tree_to_latex(tree: Tree, *, copy_to_clipboard: bool = False) -> str:
     """Convert a tree to a `qtree` latex tree.
 
